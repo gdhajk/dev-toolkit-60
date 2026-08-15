e95520a737ef6cdf3e6f5c276d0617d2dd7c22a7
Tereza Dvořáková <kaidentremblay911@gmail.com>
2026-08-15 15:14:13 +0200

    Optimize logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,24 +1,25 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Configure logger settings
-def setup_logger(log_file='app.log', max_bytes=10*1024*1024, backup_count=5):
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
+    """
+    Set up a rotating logger.
+
+    Parameters:
+    log_file (str): The name of the log file.
+    max_bytes (int): Maximum size of the log file before rotation.
+    backup_count (int): Number of backup log files to keep.
+    """
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     
-    # Create a rotating file handler
     handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    handler.setLevel(logging.DEBUG)
-    
-    # Create a formatter and set it for the handler
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-    
-    # Add the handler to the logger
     logger.addHandler(handler)
+    
     return logger
 
-# Example usage of setup_logger
 if __name__ == '__main__':
-    logger = setup_logger()
-    logger.info('Logger is set up successfully!')
+    log = setup_logger()
+    log.info('Logger is set up and ready to go!')
