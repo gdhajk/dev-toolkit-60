098bb8311afcc2dadcd617c666244f1bdfb3ff02
Tereza Dvořáková <kaidentremblay911@gmail.com>
2026-08-19 15:15:36 +0200

    Refactor click logic and structure

--- core.py
+++ core.py
@@ -1,24 +1,32 @@
 import time
+from threading import Thread
+from pynput.mouse import Button, Controller
 
 class AutoClicker:
-    def __init__(self, interval):
-        self.interval = interval
+    def __init__(self, interval=0.1):
+        self.interval = interval  
         self.running = False
+        self.mouse = Controller()
 
-    def start(self):
+    def start_clicking(self):
         self.running = True
-        print('AutoClicker started')
         while self.running:
-            self.click()
+            self.mouse.click(Button.left)
             time.sleep(self.interval)
 
-    def click(self):
-        print('Click!')  # Simulate a mouse click
-
-    def stop(self):
+    def stop_clicking(self):
         self.running = False
-        print('AutoClicker stopped')
 
-    def set_interval(self, new_interval):
-        self.interval = new_interval
-        print(f'Interval set to {self.interval} seconds')
+    def toggle(self):
+        if not self.running:
+            Thread(target=self.start_clicking).start()
+        else:
+            self.stop_clicking()
+
+if __name__ == '__main__':
+    clicker = AutoClicker(interval=0.1)
+    try:
+        clicker.toggle()  # Starts clicking
+        time.sleep(5)  # Click for 5 seconds
+    finally:
+        clicker.stop_clicking()  # Ensure clicking stops
